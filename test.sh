40edc28663af0fbc6ddde2b9793bad665bd9d51d
Dave Clarke <dave@it.uu.se>
2017-06-28 10:37:31 +0200

    Improved the code

---
+++ test.sh
@@ -0,0 +1,4 @@
+
+./Main report_suite/bart11.shuffled.cnf
+./Main -s 3 -heu 2 6 3 6 4 6 -ccm 2 report_suite/dp04s04.shuffled.cnf
+./Main -s 5 -ccl 0 report_suite/8cnf20_2400000_2400000_1.shuffled.cnf
